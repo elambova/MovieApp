cc0b4d29cc469a8291a08dbc9dd273484d071c44
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-08 15:06:14 +0300

    Add new conponents (Form and Result) and Spinner (show is when request is made and hide when is finish).

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ResultContainer from "./ResultContainer";
+import Form from "./Form";
+import Loader from "react-loader-spinner";
 
 export default class Content extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+  };
   render() {
-    return <div>Content</div>;
+    const { movies, isSubmit, handleSubmit } = this.props;
+    console.log(Object.values(movies).length);
+    return (
+      <div id="content">
+        <Form handleSubmit={handleSubmit} />
+        <div id="result">
+          {isSubmit !== true ? (
+            Object.values(movies).length > 0 && (
+              <ResultContainer movies={movies} />
+            )
+          ) : (
+            <Loader type="Oval" color="teal" height={80} width={80} />
+          )}
+        </div>
+      </div>
+    );
   }
 }
