a2c2fc047042c2d491681d91358691d58a6cd818
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-08 21:46:35 +0300

    Remove spinner and ResultContainer is view when movies data is returned.

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,28 +1,22 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import ResultContainer from "./ResultContainer";
 import Form from "./Form";
-import Loader from "react-loader-spinner";
+import ResultContainer from "./ResultContainer";
 
 export default class Content extends Component {
   static propTypes = {
     handleSubmit: PropTypes.func.isRequired,
   };
+
   render() {
-    const { movies, isSubmit, handleSubmit } = this.props;
-    console.log(Object.values(movies).length);
+    const { movies, handleSubmit, load } = this.props;
+
     return (
       <div id="content">
-        <Form handleSubmit={handleSubmit} />
-        <div id="result">
-          {isSubmit !== true ? (
-            Object.values(movies).length > 0 && (
-              <ResultContainer movies={movies} />
-            )
-          ) : (
-            <Loader type="Oval" color="teal" height={80} width={80} />
-          )}
-        </div>
+        <Form handleSubmit={handleSubmit} movies={movies} load={load} />
+        {Object.values(movies).length > 0 && (
+          <ResultContainer movies={movies} />
+        )}
       </div>
     );
   }
