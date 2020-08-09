6f6ec397fd529cc3a3775f05f5910944e3f5e1e5
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-09 23:48:59 +0300

    Remove spinner from button, add new tags for new type of search

--- src/Components/ResultContainer.js
+++ src/Components/ResultContainer.js
@@ -2,21 +2,33 @@ import React, { Component } from "react";
 
 export default class ResultContainer extends Component {
   render() {
-    const { movies } = this.props;
-    const secure_url = movies.images.secure_base_url + "w185/";
-    
+    const { data } = this.props;
+    const dataLenght = Object.values(data).length;
+    console.log(data);
+    const secure_url =
+      dataLenght !== 0 ? data.images.secure_base_url + "w185/" : "";
+
     return (
       <React.Fragment>
         <div className="flex-container">
-          {movies.results.map(
-            (movie) =>
-              movie.poster_path !== null && (
-                <div key={movie.id} id={movie.id}>
-                  <p>{movie.title}</p>
-                  <img src={`${secure_url}${movie.poster_path}`} />
+          {dataLenght > 0 && data.results[0].name !== undefined ? (
+            <div>
+              <h3 id="name">{data.results[0].name}</h3>
+              <img
+                src={`${secure_url}${data.results[0].profile_path}`}
+                alt=""
+              />
+            </div>
+          ) : null}
+          {dataLenght > 0 &&
+            data.results.map((item) =>
+              item.poster_path !== null || item.poster_path !== undefined ? (
+                <div key={item.id} id={item.id}>
+                  <p>{item.title}</p>
+                  <img src={`${secure_url}${item.poster_path}`} />
                 </div>
-              )
-          )}
+              ) : null
+            )}
         </div>
       </React.Fragment>
     );
