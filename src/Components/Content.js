5ba7a21940ee3e07660ed7f5351d14d10b37a0fa
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-09 23:53:13 +0300

    Remove Form component, now form is Content component, add spinner (show when form is submited and hide when server return data)

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,21 +1,57 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Form from "./Form";
 import ResultContainer from "./ResultContainer";
+import Loader from "react-loader-spinner";
 
 export default class Content extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: "",
+    };
+  }
+
   static propTypes = {
     handleSubmit: PropTypes.func.isRequired,
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.props.handleSubmit) {
+      this.props.handleSubmit(this.state.data);
+    }
+  };
+
   render() {
-    const { movies, handleSubmit, load } = this.props;
+    const { data, loading } = this.props;
 
     return (
       <div id="content">
-        <Form handleSubmit={handleSubmit} movies={movies} load={load} />
-        {Object.values(movies).length > 0 && (
-          <ResultContainer movies={movies} />
+        <form method="POST" onSubmit={this.onSubmit} id="data-form">
+          <div>
+            <label htmlFor="data">Please enter name for search:</label>
+            <input
+              type="text"
+              placeholder="Movie"
+              name="data"
+              id="data"
+              onChange={(e) => this.setState({ data: e.target.value })}
+              required
+            />
+          </div>
+          <input
+            type="submit"
+            form="data-form"
+            id="submit-btn"
+            name="submit"
+            value="Enter"
+          />
+        </form>
+        {loading ? (
+          <Loader type="ThreeDots" color="#02588a" height={80} width={80} />
+        ) : (
+          <ResultContainer data={data} />
         )}
       </div>
     );
