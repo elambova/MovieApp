32ec043926b5753cf82c090ca639eddebd4f985c
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-10 22:29:23 +0300

    Add functionality for differect components. If media_type is movie or tv - load Movies Component, if is person - load People Component.

--- src/Components/ResultContainer.js
+++ src/Components/ResultContainer.js
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
 import Movies from "./results/Movies";
 import People from "./results/People";
-import TvShows from "./results/TvShows";
 
 export default class ResultContainer extends Component {
+  getMediaType = (data) => {
+    const movies = [];
+    const tv = [];
+    const people = [];
+    const secure_url = data.imagesUrl;
+
+    data.apiData.forEach((item) => {
+      switch (item.media_type) {
+        case "movie":
+          movies.push(item);
+        case "person":
+          people.push(item);
+        case "tv":
+          tv.push(item);
+      }
+    });
+
+    if (movies.length > 0 || tv.length > 0) {
+      return <Movies secure_url={secure_url} tv={tv} />;
+    } else {
+      return <People secure_url={secure_url} people={people} />;
+    }
+  };
+
   render() {
     const { data } = this.props;
     const dataLenght = Object.values(data).length;
-    const secure_url =
-      dataLenght !== 0 ? data.images.secure_base_url + "w185/" : "";
 
     return (
       <React.Fragment>
         <div className="flex-container">
-          {dataLenght > 0 &&
-            (() => {
-              switch (data.results[0].media_type) {
-                case "person":
-                  return <People people={data} secure_url={secure_url} />;
-                case "movie":
-                  return <Movies movies={data} secure_url={secure_url} />;
-                case "tv":
-                  return <TvShows tv={data} secure_url={secure_url} />;
-                default:
-                  return null;
-              }
-            })()}
+          {dataLenght > 0 && this.getMediaType(data)}
         </div>
       </React.Fragment>
     );
