4710f74f0d97e2e5085d35d8b94c2e900ac07a7f
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-10 10:48:56 +0300

    Add switch statement for diferent data return from API

--- src/Components/ResultContainer.js
+++ src/Components/ResultContainer.js
@@ -1,34 +1,31 @@
 import React, { Component } from "react";
+import Movies from "./results/Movies";
+import People from "./results/People";
+import TvShows from "./results/TvShows";
 
 export default class ResultContainer extends Component {
   render() {
     const { data } = this.props;
     const dataLenght = Object.values(data).length;
-    console.log(data);
     const secure_url =
       dataLenght !== 0 ? data.images.secure_base_url + "w185/" : "";
 
     return (
       <React.Fragment>
         <div className="flex-container">
-          {dataLenght > 0 && data.results[0].name !== undefined ? (
-            <div>
-              <h3 id="name">{data.results[0].name}</h3>
-              <img
-                src={`${secure_url}${data.results[0].profile_path}`}
-                alt=""
-              />
-            </div>
-          ) : null}
           {dataLenght > 0 &&
-            data.results.map((item) =>
-              item.poster_path !== null || item.poster_path !== undefined ? (
-                <div key={item.id} id={item.id}>
-                  <p>{item.title}</p>
-                  <img src={`${secure_url}${item.poster_path}`} />
-                </div>
-              ) : null
-            )}
+            (() => {
+              switch (data.results[0].media_type) {
+                case "person":
+                  return <People people={data} secure_url={secure_url} />;
+                case "movie":
+                  return <Movies movies={data} secure_url={secure_url} />;
+                case "tv":
+                  return <TvShows tv={data} secure_url={secure_url} />;
+                default:
+                  return null;
+              }
+            })()}
         </div>
       </React.Fragment>
     );
