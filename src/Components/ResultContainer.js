2691244209335de123faef75efd93c8c89b9d22a
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-11 09:22:04 +0300

    Change conditions for load components.

--- src/Components/ResultContainer.js
+++ src/Components/ResultContainer.js
@@ -5,25 +5,33 @@ import People from "./results/People";
 export default class ResultContainer extends Component {
   getMediaType = (data) => {
     const movies = [];
-    const tv = [];
     const people = [];
     const secure_url = data.imagesUrl;
 
     data.apiData.forEach((item) => {
       switch (item.media_type) {
         case "movie":
-          movies.push(item);
+          return movies.push(item);
         case "person":
-          people.push(item);
+          return people.push(item);
         case "tv":
-          tv.push(item);
+          return movies.push(item);
       }
     });
 
-    if (movies.length > 0 || tv.length > 0) {
-      return <Movies secure_url={secure_url} tv={tv} />;
-    } else {
+    if (people.length > 0 && movies.length > 0) {
+      return (
+        <React.Fragment>
+          <People secure_url={secure_url} people={people} />
+          <Movies secure_url={secure_url} movies={movies} />
+        </React.Fragment>
+      );
+    } else if (movies.length > 0) {
+      return <Movies secure_url={secure_url} movies={movies} />;
+    } else if (people.length > 0) {
       return <People secure_url={secure_url} people={people} />;
+    } else {
+      return <h3>No results found for your search!</h3>;
     }
   };
 
