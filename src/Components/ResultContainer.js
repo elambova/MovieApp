c114bc7cae1dfcbb80a4a93dbac8a68d4d123acb
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-12 10:16:46 +0300

    Add handleClick props to two results component - Movies and People

--- src/Components/ResultContainer.js
+++ src/Components/ResultContainer.js
@@ -3,7 +3,7 @@ import Movies from "./results/Movies";
 import People from "./results/People";
 
 export default class ResultContainer extends Component {
-  getMediaType = (data) => {
+  renderComponentsByMediaType = (data, handleClick) => {
     const movies = [];
     const people = [];
     const secure_url = data.imagesUrl;
@@ -22,29 +22,50 @@ export default class ResultContainer extends Component {
     if (people.length > 0 && movies.length > 0) {
       return (
         <React.Fragment>
-          <People secure_url={secure_url} people={people} />
-          <Movies secure_url={secure_url} movies={movies} />
+          <People
+            secure_url={secure_url}
+            handleClick={handleClick}
+            people={people}
+          />
+          <Movies
+            secure_url={secure_url}
+            handleClick={handleClick}
+            movies={movies}
+          />
         </React.Fragment>
       );
     } else if (movies.length > 0) {
-      return <Movies secure_url={secure_url} movies={movies} />;
+      return (
+        <Movies
+          secure_url={secure_url}
+          handleClick={handleClick}
+          movies={movies}
+        />
+      );
     } else if (people.length > 0) {
-      return <People secure_url={secure_url} people={people} />;
+      return (
+        <People
+          secure_url={secure_url}
+          handleClick={handleClick}
+          people={people}
+        />
+      );
     } else {
       return <h3>No results found for your search!</h3>;
     }
   };
 
   render() {
-    const { data } = this.props;
+    const { data, handleClick } = this.props;
     const dataLenght = Object.values(data).length;
 
     return (
-      <React.Fragment>
+      <div className="content">
         <div className="flex-container">
-          {dataLenght > 0 && this.getMediaType(data)}
+          {dataLenght > 0 &&
+            this.renderComponentsByMediaType(data, handleClick)}
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
