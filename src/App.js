0c515b703680e5c79a76c1129d6ac0e60442d30f
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-12 23:00:20 +0300

    Add new handlers for person and tv. Add handler for change redirectToReferrer to false (this is for go back to previos route).

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ import Footer from "./Components/Footer";
 import * as connect from "./ConnectionAPI/connect";
 import ResultContainer from "./Components/ResultContainer";
 import Movie from "./Components/Movie/Movie";
+import Tv from "./Components/Movie/Tv";
+import Person from "./Components/Movie/Person";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [], movie: [], redirectToReferrer: false };
+    this.state = {
+      data: [],
+      movie: [],
+      tv: [],
+      person: [],
+      redirectToReferrer: false,
+    };
   }
 
   handleSubmit = async (data) => {
@@ -24,55 +32,95 @@ class App extends Component {
     });
   };
 
-  handleClick = async (id) => {
+  handleClickMovie = async (id) => {
     await connect
       .getDataMovie(id)
       .then((movie) => this.setState({ movie: movie }));
-    <Redirect to="/movie/:id" />;
+  };
+
+  handleClickTv = async (id) => {
+    await connect.getDataTv(id).then((tv) => this.setState({ tv: tv }));
+  };
+
+  handleClickPerson = async (id) => {
+    await connect
+      .getDataPerson(id)
+      .then((person) => this.setState({ person: person }));
+  };
+
+  handleClickReferrer = () => {
+    this.setState({ redirectToReferrer: false });
   };
 
   render() {
-    const { data, movie, redirectToReferrer } = this.state;
-
+    const { data, movie, tv, person, redirectToReferrer } = this.state;
     return (
       <Router>
         <React.Fragment>
           <Header />
-          {!redirectToReferrer ? (
-            <Route
-              exact
-              path="/"
-              render={() => <Form handleSubmit={this.handleSubmit} />}
-            />
-          ) : (
-            <React.Fragment>
-              {Object.values(data).length > 0 && (
-                <React.Fragment>
-                  <Redirect from="/" to="/result" />
-                  <Route
-                    exact
-                    path="/result"
-                    render={() => (
-                      <ResultContainer
-                        handleClick={this.handleClick}
-                        data={data}
-                      />
-                    )}
-                  />
-                </React.Fragment>
-              )}
-              {Object.values(movie).length > 0 && (
-                <React.Fragment>
-                  <Redirect from="/result" to={`movie/:${movie.apiData.id}`} />
-                  <Route
-                    exact
-                    path={`/movie/:${movie.apiData.id}`}
-                    render={() => <Movie movie={movie} />}
-                  />
-                </React.Fragment>
-              )}
-            </React.Fragment>
-          )}
+          <Switch>
+            {!redirectToReferrer ? (
+              <Route
+                exact
+                path="/"
+                render={() => <Form handleSubmit={this.handleSubmit} />}
+              />
+            ) : (
+              <React.Fragment>
+                {Object.values(data).length > 0 && (
+                  <React.Fragment>
+                    <Redirect from="/" to="/result" />
+                    <Route
+                      exact
+                      path="/result"
+                      render={() => (
+                        <ResultContainer
+                          handleClickMovie={this.handleClickMovie}
+                          handleClickTv={this.handleClickTv}
+                          handleClickPerson={this.handleClickPerson}
+                          handleClickReferrer={this.handleClickReferrer}
+                          data={data}
+                        />
+                      )}
+                    />
+                  </React.Fragment>
+                )}
+                {Object.values(movie).length > 0 && (
+                  <React.Fragment>
+                    <Redirect from="/result" to={`movie/${movie.apiData.id}`} />
+                    <Route
+                      exact
+                      path={`/movie/${movie.apiData.id}`}
+                      render={() => <Movie movie={movie} />}
+                    />
+                  </React.Fragment>
+                )}
+                {Object.values(tv).length > 0 && (
+                  <React.Fragment>
+                    <Redirect from="/result" to={`tv/${tv.apiData.id}`} />
+                    <Route
+                      exact
+                      path={`/tv/${tv.apiData.id}`}
+                      render={() => <Tv tv={tv} />}
+                    />
+                  </React.Fragment>
+                )}
+                {Object.values(person).length > 0 && (
+                  <React.Fragment>
+                    <Redirect
+                      from="/result"
+                      to={`person/${person.connectApi.id}`}
+                    />
+                    <Route
+                      exact
+                      path={`/person/${person.connectApi.id}`}
+                      render={() => <Person person={person} />}
+                    />
+                  </React.Fragment>
+                )}
+              </React.Fragment>
+            )}
+          </Switch>
           <Footer />
         </React.Fragment>
       </Router>
