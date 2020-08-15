c386533cab663e7653ad501d0665a33dd68feaf5
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-15 23:49:11 +0300

    Add two H2 - Person and Movies

--- src/Components/results/People.js
+++ src/Components/results/People.js
@@ -34,6 +34,7 @@ function People(props) {
     <React.Fragment>
       {peopleLenght > 0 && (
         <div id="person-info">
+          <h2>Person</h2>
           <Link
             className="link"
             to={`/person/${people[0].id}`}
@@ -44,45 +45,48 @@ function People(props) {
           </Link>
         </div>
       )}
-      {peopleLenght > 0 &&
-        people[0].known_for.length > 0 &&
-        people.map((person) =>
-          person.known_for.map((movie) =>
-            movie.media_type === "movie" ? (
-              <Link
-                className="link"
-                to={`/movie/${movie.id}`}
-                key={movie.id}
-                onClick={() => clickMovie(movie.id)}
-              >
-                <p>{movie.title ? movie.title : movie.original_name}</p>
-                <img
-                  src={
-                    movie.poster_path === null
-                      ? `${noPhotoFound}`
-                      : `${secure_url}${movie.poster_path}`
-                  }
-                />
-              </Link>
-            ) : (
-              <Link
-                className="link"
-                to={`/tv/${movie.id}`}
-                key={movie.id}
-                onClick={() => clickTv(movie.id)}
-              >
-                <p>{movie.title ? movie.title : movie.original_name}</p>
-                <img
-                  src={
-                    movie.poster_path === null
-                      ? `${noPhotoFound}`
-                      : `${secure_url}${movie.poster_path}`
-                  }
-                />
-              </Link>
+      <React.Fragment>
+        <h2>Movies</h2>
+        {peopleLenght > 0 &&
+          people[0].known_for.length > 0 &&
+          people.map((person) =>
+            person.known_for.map((movie) =>
+              movie.media_type === "movie" ? (
+                <Link
+                  className="link"
+                  to={`/movie/${movie.id}`}
+                  key={movie.id}
+                  onClick={() => clickMovie(movie.id)}
+                >
+                  <p>{movie.title ? movie.title : movie.original_name}</p>
+                  <img
+                    src={
+                      movie.poster_path === null
+                        ? `${noPhotoFound}`
+                        : `${secure_url}${movie.poster_path}`
+                    }
+                  />
+                </Link>
+              ) : (
+                <Link
+                  className="link"
+                  to={`/tv/${movie.id}`}
+                  key={movie.id}
+                  onClick={() => clickTv(movie.id)}
+                >
+                  <p>{movie.title ? movie.title : movie.original_name}</p>
+                  <img
+                    src={
+                      movie.poster_path === null
+                        ? `${noPhotoFound}`
+                        : `${secure_url}${movie.poster_path}`
+                    }
+                  />
+                </Link>
+              )
             )
-          )
-        )}
+          )}
+      </React.Fragment>
     </React.Fragment>
   );
 }
