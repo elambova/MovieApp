85f2cfcd4670d866ce185d88481105ce9a887428
Elena Lambova <elena.g.lambova@gmail.com>
2020-08-17 15:44:53 +0300

    Add new component for single episode from season in Tv.

---
+++ src/Components/SingleResult/Episode.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Episode(props) {
+  console.log(props);
+  return <div>Episode</div>;
+}
+
+Episode.propTypes = {};
+
+export default Episode;
